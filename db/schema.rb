13504828fac7054fd72313af43ce0b9dd6a59e79
r-ichinose <reoyinolai719@gmail.com>
2024-11-12 16:42:02 +0900

    ユーザー認証機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2024_11_12_051855) do
-  create_table "users", force: :cascade do |t|
+  create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
